bc8e319ce222c317ffb95f757b5eb3d43b9bb9e1
RiyadHassen <riyadworku@gmail.com>
2021-06-17 05:50:22 +0300

    service and fk added

--- src/arm_gazebo/scripts/ikservice.py
+++ src/arm_gazebo/scripts/ikservice.py
@@ -5,12 +5,12 @@ import rospy
 def Handle_IK(req):
     desired_pose = req.desired_pose
     arm = tk.Actuator([
-        'z', [0, 0, 0.15], #chasis_arm1
-        'x', [0, 0, 2.0],  #arm1_arm2
-        'x', [0, 0, 1.0],  #arm2_arm3
-        'x', [0, 0, 0.5],  #arm3_arm4
-        'z', [0, 0, 0.1],  #arm4_arm5
-        'y', [0, 0, 0.1],  #arm5_palm
+        'z', [0, 0, 0.15], 
+        'x', [0, 0, 2.0],  
+        'x', [0, 0, 1.0],  
+        'x', [0, 0, 0.5],  
+        'z', [0, 0, 0.1],  
+        'y', [0, 0, 0.1],  
     ])
     arm.ee = [desired_pose[0], desired_pose[1], desired_pose[2]]
     return ikResponse(arm.angles)
