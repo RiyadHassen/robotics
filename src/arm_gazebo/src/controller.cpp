c0b63ca8c357c6fa006cd1299517a1d903b0bc20
RiyadHassen <riyadworku@gmail.com>
2021-06-17 06:01:22 +0300

    update angles added

--- src/arm_gazebo/src/controller.cpp
+++ src/arm_gazebo/src/controller.cpp
@@ -4,6 +4,10 @@
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 #include <iostream>
+#include <thread>
+#include "ros/ros.h"
+#include "arm_gazebo/ik.h"
+#include "arm_gazebo/fk.h"
 namespace gazebo
 {
 	class ModelPush : public ModelPlugin
@@ -33,7 +37,17 @@ namespace gazebo
 		  	this->SetPID(joint2, pid);
 			this->SetPID(joint3, pid);
 			this->SetPID(joint4, pid);
-		  	
+			if (!ros::isInitialized())
+			{
+				int argc = 0;
+				char **argv = NULL;
+				ros::init(argc, argv, "gazebo_client",
+						  ros::init_options::NoSigintHandler);
+			}
+			this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+			
+			
 			
 			
 			// Listen to the update event. This event is broadcast every
@@ -43,6 +57,7 @@ namespace gazebo
 
 		
 		}
+		
 
 		// Called by the world update start event
 	public:
@@ -95,6 +110,47 @@ namespace gazebo
 			this->SetAngle(palmjoinleft, 1);
 			this->SetPID(palmjointright, pid);
 			this->SetAngle(palmjointright, -1);
+		}
+
+		private:
+		void UpdateJointAngles(std::vector<double> _pose)
+		{
+			std::vector<double> joint_angles = this->InverseKinematics(_pose);
+
+			std::string joint0 = this->model->GetJoint("chasis_arm1_joint")->GetScopedName();
+			std::string joint1 = this->model->GetJoint("arm1_arm2_joint")->GetScopedName();
+			std::string joint2 = this->model->GetJoint("arm2_arm3_joint")->GetScopedName();
+			std::string joint3 = this->model->GetJoint("arm3_arm4_joint")->GetScopedName();
+			std::string joint4 = this->model->GetJoint("arm4_arm5_joint")->GetScopedName();
+			std::string joint5 = this->model->GetJoint("palm_riser")->GetScopedName();
+
+
+			common::PID joint1PID = common::PID(30.0, 10.0, 7.0);
+			common::PID joint2PID = common::PID(25.0, 8.0, 6.0);
+			common::PID joint3PID = common::PID(20.0, 7.0, 5.0);
+			common::PID joint4PID = common::PID(17.0, 6.0, 4.0);
+			common::PID joint5PID = common::PID(15.0, 5.0, 3.0);
+			common::PID joint6PID = common::PID(10.0, 4.0, 2.0);
+
+			this->SetPID(joint0, joint1PID);
+			this->SetAngle(joint0, joint_angles[0]);
+
+			this->SetPID(joint1, joint2PID);
+			this->SetAngle(joint1, joint_angles[1]);
+
+			this->SetPID(joint2, joint3PID);
+			this->SetAngle(joint2, joint_angles[2]);
+
+			this->SetPID(joint3, joint4PID);
+			this->SetAngle(joint3, joint_angles[3]);
+
+			this->SetPID(joint4, joint5PID);
+			this->SetAngle(joint4, joint_angles[4]);
+
+			this->SetPID(joint5, joint6PID);
+			this->SetAngle(join5, joint_angles[5]);
+
+			this->jointController->Update();
 		}
 
 	private:
